cf29977fb367e3871ff55663a3b44d3f2500cc68
Drake <drakefreeman@gmail.com>
2025-07-11 17:22:55 -0600

    Add new and updated project files

---
+++ Source/NewThing/DeconstructWork.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace Replace_Stuff.NewThing
+{
+	[HarmonyPatch(typeof(Frame), "WorkToBuild", MethodType.Getter)]
+	public static class NewThingDeconstructWork
+	{
+		//public float WorkToBuild
+		public static void Postfix(Frame __instance, ref float __result)
+		{
+			if (__instance.IsNewThingReplacement(out Thing oldThing))
+				__result += ReplaceFrame.WorkToDeconstructDef(oldThing.def, oldThing.Stuff);
+		}
+	}
+	[HarmonyPatch(typeof(Blueprint_Build), "WorkTotal", MethodType.Getter)]
+	public static class NewThingDeconstructWork_Blueprint
+	{
+		//public float WorkToBuild
+		public static void Postfix(Frame __instance, ref float __result)
+		{
+			if (__instance.IsNewThingReplacement(out Thing oldThing))
+				__result += ReplaceFrame.WorkToDeconstructDef(oldThing.def, oldThing.Stuff);
+		}
+	}
+}
---
+++ Source/Utilities/RemoveThingFromStatWorkerCache.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Replace_Stuff.Utilities
+{
+	public static class RemoveThingFromStatWorkerCache
+	{
+		//class StatDef {
+		//private StatWorker workerInt;
+		public static AccessTools.FieldRef<StatDef, StatWorker> workerInt = AccessTools.FieldRefAccess<StatDef, StatWorker>("workerInt");
+
+		//class StatWorker {
+		//private Dictionary<Thing, StatCacheEntry> temporaryStatCache;
+		//private Dictionary<Thing, float> immutableStatCache;
+		public static AccessTools.FieldRef<StatWorker, Dictionary<Thing, StatCacheEntry>> temporaryStatCache = AccessTools.FieldRefAccess<StatWorker, Dictionary<Thing, StatCacheEntry>>("temporaryStatCache");
+		public static AccessTools.FieldRef<StatWorker, ConcurrentDictionary<Thing, float>> immutableStatCache = AccessTools.FieldRefAccess<StatWorker, ConcurrentDictionary<Thing, float>>("immutableStatCache");
+
+		public static void RemoveFromStatWorkerCaches(this Thing thing)
+		{
+			foreach (StatDef statDef in DefDatabase<StatDef>.AllDefsListForReading)
+			{
+				// Corrected line: Use the delegate to access the private field.
+				var worker = workerInt(statDef);
+				if (worker != null)
+				{
+					// Corrected lines: Use the delegates to access the private fields.
+					temporaryStatCache(worker)?.Remove(thing);
+					immutableStatCache(worker)?.TryRemove(thing, out _);
+				}
+			}
+		}
+	}
+}
